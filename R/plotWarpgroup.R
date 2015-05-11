b6f5a943608236251cf2053b17416fee851b6b49
Nathaniel Mahieu <nate@mahie.us>
2015-05-11 16:51:55 -0500

    Plotting Functionality - Still needs cleaned up

--- R/plotWarpgroup.R
+++ R/plotWarpgroup.R
@@ -15,6 +15,73 @@ plot.warpgroup = function(group, eic.mat.s, scan.start) {
     facet_wrap(~sample, scales="free_y") + theme_bw()+ theme(legend.position="none")
 }
 
+
+plot.warpgroup = function(group, xs, xr.l, sc.pad = 20, mz.pad = 0.001) {
+  g = group
+  ps = xs@peaks[g[,"pn"],,drop=F]
+  ps[,c("sc","scmin","scmax","sample")] = g[,c("sc","scmin","scmax","sample")]
+  
+  eic.mat = makeEicMat(ps, xr.l, sc.pad, mz.pad)
+
+  g[,c("sc", "scmin", "scmax")] = g[,c("sc", "scmin", "scmax")] - min(g[,"scmin"]) + 1 + sc.pad
+  plot_peaks_bounds(eic.mat, g)
+}
+
+
+plot_peaks_bounds = function(eic.mat.s, bounds) {
+  eic.mat.s = apply(eic.mat.s, 2, function(x) { x/max(x) })
+  eic.mat.s = sapply(seq(ncol(eic.mat.s)), function(i) { eic.mat.s[,i]+1.1*(i-1) })
+  
+  df = melt(eic.mat.s)
+  colnames(df) = c("Scan", "Sample", "Intensity")
+  
+  int = array(logical(), dim=dim(eic.mat.s)); int[] = F
+  for (r in seq(nrow(bounds))) int[floor(bounds[r,"scmin"]):floor(bounds[r,"scmax"]), bounds[r,"sample"]] = T
+  df.int = melt(int)
+  colnames(df.int) = c("Scan", "Sample", "Integrated")
+  
+  df.int = data.frame(df, Integrated = melt(int)[,3])
+  
+  df.bounds = melt(data.frame(bounds), id.vars = "sample", measure.vars=c("scmin", "scmax"))
+  colnames(df.bounds) = c("Sample", "Bound", "Scan")
+  Intensity = aaply(as.matrix(df.bounds[,c("Sample", "Scan")]), 1, function(x) {
+    eic.mat.s[x["Scan"],x["Sample"]]
+    })
+  df.bounds = cbind(df.bounds, Intensity)
+  
+  if(F) {
+  ggplot(df.int, aes(y = Intensity, x = Scan)) + 
+    geom_line() +
+    geom_line(data=subset(df.int, Integrated==T), mapping=aes(colour="red")) +
+    geom_point(data=df.bounds, aes(y=1)) + 
+    facet_wrap(~Sample, ncol=2, scales="free_y") +
+    theme(legend.position="none", axis.text.y=element_blank(), axis.ticks=element_blank()) +
+    theme(#axis.line=element_blank(),
+      axis.text.x=element_blank(),
+      axis.text.y=element_blank(),
+      axis.ticks=element_blank(),
+      legend.position="none",
+      panel.background=element_blank(),
+      panel.border=element_blank(),
+      panel.grid.major=element_blank(),
+      panel.grid.minor=element_blank(),
+      plot.background=element_blank())
+}
+  
+  ggplot(df.int, aes(y = Intensity, x = Scan, group=factor(Sample))) + 
+    geom_line(colour="#a0a0a0") +
+    geom_line(data=subset(df.int, Integrated==T), mapping=aes(group=factor(Sample)), colour="#D63647", size=1) + 
+    geom_point(data=df.bounds, colour="#D63647", size=4) +
+    theme(
+      axis.text.y=element_blank(),
+      axis.ticks=element_blank(),
+      legend.position="none",
+      panel.background=element_blank(),
+      panel.grid.minor=element_blank(),
+      panel.grid.major=element_line(colour="#F2EBEC")
+      )
+}
+
 plot.warpgroup = function(prepath=".", ps, cm.mem, eic.mat, eic.mat.s, p.sc.params) {
   
   foo = merge(ps, data.frame(cm.mem, n=names(cm.mem)), by = "n", all=T)    
--- R/plotXcms.R
+++ R/plotXcms.R
@@ -2,27 +2,30 @@ plotGroup.xs = function(i, xs, xr.l, sc.pad = 20, mz.pad = 0) {
   g = xs@groupidx[[i]]
   ps = xs@peaks[g,,drop=F]
   
+  ps = ps[order(ps[,"sample"]),]
+  
+  eic.mat = makeEicMat(ps, xr.l, sc.pad = sc.pad, mz.pad = mz.pad)
+  
+  bounds = ps[,c("scmin","scmax", "sample")]; bounds[,1:2] = bounds[,1:2] - min(scan.range) +1
+
+  plot_peaks_bounds(eic.mat, na.omit(bounds))
+}
+
+makeEicMat = function(ps, xr.l, sc.pad = 20, mz.pad = 0) {
+  
+  sc.start = min(ps[,"scmin"], na.rm=T) + 1
+  
   scan.range = c(floor(min(ps[,"scmin"], na.rm=T))-sc.pad, ceiling(max(ps[,"scmax"], na.rm=T))+sc.pad)
   eic.mat = matrix(numeric(), ncol = nrow(ps), nrow = scan.range[2]-scan.range[1]+1)
   
+  mz.rangeg = unname(c(min(ps[,"mzmin"]-mz.pad, na.rm=T), max(ps[,"mzmax"]+mz.pad, na.rm=T)))
+  
   for (j in seq(ps[,1])) {
+    if (is.na(ps[j,"mzmin"])) {mz.range = mz.rangeg} else {
     mz.range = unname(c(ps[j,"mzmin"]-mz.pad, ps[j,"mzmax"]+mz.pad))
+    }
     eic.mat[,j] = rawEIC(xr.l[[ps[j,"sample"]]], mzrange = mz.range, scanrange = scan.range)$intensity 
   }
   
-  plot.me = melt(eic.mat)
-  colnames(plot.me) = c("scan", "peak", "int")
-  plot.me[,"peak"] = factor(plot.me[,"peak"])
-  
-  annos = data.frame(ps)
-  annos[,"n"] = seq(annos[,1])
-  colnames(annos)[c(9, 11, 12, 13)] = c("peak","scwm", "scmax", "scmin")
-  annos[,c(11, 12, 13)] = annos[,c(11, 12, 13)] - min(scan.range)
-  
-  ggplot() + 
-    geom_path(data = plot.me, aes(y=int, x= scan, colour=peak)) + 
-    geom_point(data=annos, mapping=aes(y=0, x=scmax), colour="#000000") + 
-    geom_point(data=annos, mapping=aes(y=0, x=scmin), colour="#000000") +
-    geom_vline(data=annos, mapping=aes(xintercept=scwm), colour="#000000", alpha=0.3)+
-    facet_grid(peak ~ ., scales="free_y")
+  eic.mat
 }
